c6c1229c15474e573e102c0725cab2283fcc05d1
root <ZJLwan520>
2019-11-08 14:35:37 +0800

    提交享元模式

---
+++ src/main/java/com/java/mode/flyWeight/EmployeeReportManager.java
@@ -0,0 +1,25 @@
+package com.java.mode.flyWeight;
+
+/**
+ * 员工报表
+ * 具体享元类
+ *
+ * @author : Gary
+ * @since 2019/11/08 14:23
+ */
+public class EmployeeReportManager implements IReportManager {
+    /**
+     * 租户ID
+     */
+    protected String tenantId = null;
+
+    public EmployeeReportManager(String tenantId) {
+        System.out.println("EmployeeReportManager is create!!!");
+        this.tenantId = tenantId;
+    }
+
+    @Override
+    public String createReport() {
+        return "This is a employee report!";
+    }
+}
