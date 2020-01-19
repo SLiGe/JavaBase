087cd002f0fdeacea23046ff5b31d7d0f19a1762
root <ZJLwan520>
2020-01-19 23:53:07 +0800

    提交file学习

--- src/main/java/com/java/file/SystemDemo.java
+++ src/main/java/com/java/file/SystemDemo.java
@@ -1,5 +1,10 @@
 package com.java.file;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+
 /**
  * @author Gary
  * @since 2020/01/14 15:41
@@ -8,10 +13,21 @@ public class SystemDemo {
 
     public static void main(String[] args) {
         getSystem();
+        writeTxt();
     }
 
-    static void getSystem(){
+    private static void getSystem() {
         String property = System.getProperty("user.dir");
         System.out.println(property);
+    }
+
+    private static void writeTxt() {
+        try (OutputStream outputStream = new FileOutputStream("de.txt")) {
+            String data = "hello world!";
+            byte[] bytes = data.getBytes(Charset.forName("UTF-8"));
+            outputStream.write(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
